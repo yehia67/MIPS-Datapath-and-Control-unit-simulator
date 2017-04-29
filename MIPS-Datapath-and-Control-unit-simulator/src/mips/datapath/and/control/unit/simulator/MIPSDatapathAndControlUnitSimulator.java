15271764647282746526efad62d4b3920c8535c7
yehia67 <yehiatarek67@gmail.com>
2017-04-30 00:34:05 +0200

    el user byda5l el value bta3 el register w el 2sm bta3o

--- MIPS-Datapath-and-Control-unit-simulator/src/mips/datapath/and/control/unit/simulator/MIPSDatapathAndControlUnitSimulator.java
+++ MIPS-Datapath-and-Control-unit-simulator/src/mips/datapath/and/control/unit/simulator/MIPSDatapathAndControlUnitSimulator.java
@@ -9,13 +9,36 @@ package mips.datapath.and.control.unit.simulator;
  *
  * @author Sony
  */
+import java.util.*;
 public class MIPSDatapathAndControlUnitSimulator {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Registers[] x = new Registers[1000];
+        System.out.println("please write the registers that u want to use and it s value that hold");
+        System.out.println("if u want to use a register without initialization please press 0");
+        System.out.println("When u finish please press eof");
+        Scanner sc = new Scanner(System.in);
+        int j = 0;
+        String s;
+        while( sc.hasNextLine())
+        {   
+            System.out.print(" name of register is ..");
+            x[j].name = sc.toString();
+            System.out.println("");
+            if(x[j].name != "eof")
+            {
+                break;
+            }
+            System.out.print(" value of register is ..");
+            x[j].data = sc.nextInt();
+            System.out.println("");
+            
+            j++;
+        }
+  
     }
     
 }
