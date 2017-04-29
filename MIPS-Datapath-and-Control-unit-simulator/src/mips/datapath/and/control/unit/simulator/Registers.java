201a86a119523dd946bf1b9ac30b84afeda44d25
yehia67 <yehiatarek67@gmail.com>
2017-04-29 22:30:39 +0200

    3amlt registr bya5od int value el howa rakm el register w string l name data ll value el gowah w 3amltohm public 3lshan n acceshom bshoola
    function determinvalue msh mohma dlw2ty han5lyha m3 el datapath

--- MIPS-Datapath-and-Control-unit-simulator/src/mips/datapath/and/control/unit/simulator/Registers.java
+++ MIPS-Datapath-and-Control-unit-simulator/src/mips/datapath/and/control/unit/simulator/Registers.java
@@ -10,5 +10,97 @@ package mips.datapath.and.control.unit.simulator;
  * @author Sony
  */
 public class Registers {
-    
+   public int value;
+    public String name;
+    public int data;// value that we take from user
+  private int zero = 0;
+ public String getname()
+  {
+     return name ;
+  }
+ public void setname(String x)
+ {
+     name = x;
+ }
+public void setvalue(int x)
+{
+    value = x;
+} 
+public int getvalue()
+{
+    return value;
+}
+public int determinvalue(String x)
+{
+    if(x == "$zero" || x == "$0")
+    {
+        return  0;
+    }
+if(x == "$at")
+{
+    return 1;
+}
+if(x == "$at")
+{
+    return 1;
+}
+if(x == "$vo")
+{
+    return 2;
+}
+if(x == "$v1")
+{
+    return 3;
+}
+if(x == "$ao")
+{
+    return 4;
+}
+if(x == "$a1")
+{
+    return 5;
+}
+if(x == "$a2")
+{
+    return 6;
+}
+if(x == "$a3")
+{
+    return 7;
+}
+if(x == "$t0")
+{
+    return 8;
+}
+if(x == "$t1")
+{
+    return 9;
+}
+if(x == "$t2")
+{
+    return 10;
+}
+if(x == "$t3")
+{
+    return 11;
+}
+if(x == "$t4")
+{
+    return 12;
+}
+if(x == "$t5")
+{
+    return 13;
+}
+if(x == "$t6")
+{
+    return 14;
+}
+if(x == "$t7")
+{
+    return 12;
+}
+// zh2t hakml el ba2yy ama nygy n3mlo
+return -1;
+}
 }
