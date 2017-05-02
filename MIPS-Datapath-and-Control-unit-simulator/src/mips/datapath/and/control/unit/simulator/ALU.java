1d6aaac9bf55601910efb2bdb0e10f1d045497aa
yehia67 <yehiatarek67@gmail.com>
2017-05-02 15:02:09 +0200

    finish ALU CLass

--- MIPS-Datapath-and-Control-unit-simulator/src/mips/datapath/and/control/unit/simulator/ALU.java
+++ MIPS-Datapath-and-Control-unit-simulator/src/mips/datapath/and/control/unit/simulator/ALU.java
@@ -10,5 +10,55 @@ package mips.datapath.and.control.unit.simulator;
  * @author Sony
  */
 public class ALU {
+    String ALUOP,functioncode;
+    int firstsrc,secondsrc;
+    public ALU(String aluop,String funcod,int f, int s)
+    {
+        ALUOP = aluop;
+        functioncode = funcod;
+        firstsrc = f;
+        secondsrc = s;
+    }
+    public int opersation(String op,String func){
+        if(op == "00")    // I type
+        {
+          return firstsrc + secondsrc;
+        }
+        else if(op == "01")
+        {
+             return firstsrc - secondsrc;
+        }
+       
+        else if(func + op == "0000")
+        {
+           if(secondsrc > 0 && firstsrc > 0)
+           {
+               return 1;
+           }
+           else
+           {
+               return 0;
+           }
+        }
+        else if(func + op == "0001")
+        {
+            if(secondsrc > 0 || firstsrc > 0)
+           {
+               return 1;
+           }
+           else
+           {
+               return 0;
+           }
+        }
+        else if(func + op == "0111")
+        {
+            return firstsrc - secondsrc;
+        }
+        else 
+            return 9999; //error
+            }
+    
+    
     
 }
--- MIPS-Datapath-and-Control-unit-simulator/src/mips/datapath/and/control/unit/simulator/Memory.java
+++ MIPS-Datapath-and-Control-unit-simulator/src/mips/datapath/and/control/unit/simulator/Memory.java
@@ -5,6 +5,8 @@
  */
 package mips.datapath.and.control.unit.simulator;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Sony
@@ -12,3 +14,37 @@ package mips.datapath.and.control.unit.simulator;
 public class Memory {
     
 }
+public double finddata (Instruction x)
+{
+    for(int i = 0; i < Datamem.size(); i++)
+    {
+        if(x == instrucmem.get(i))
+        {
+            return Datamem.get(i);
+        }
+    }
+ return -1;
+}
+  public int IF(int temp)
+  {
+      if(temp > Memory.pc)
+      {
+         return -1; 
+      }
+  return pc-temp/4;
+  }
+  
+public Double load (int address)
+{
+  int index = pc-address/4;
+  return Datamem.get(index);
+}
+
+public void store(int address, Double data)
+{
+    int index = pc-address/4;
+    Datamem.set(index, data);
+}
+        
+        }
+
