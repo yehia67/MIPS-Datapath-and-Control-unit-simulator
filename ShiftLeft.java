58403ad9656b5a5195e6bdab36e3f037ae6308d4
MahmoudKhalil <mahmoudkhalil11@gmail.com>
2017-05-02 20:21:58 +0200

    Create ShiftLeft.java

---
+++ ShiftLeft.java
@@ -0,0 +1,27 @@
+package mips.datapath.and.control.unit.simulator;
+
+public class ShiftLeft {
+    private int inputOffset;
+    private int outputOffset;
+    
+    public ShiftLeft(int offset) {
+        inputOffset = offset;
+        shiftLeftTwo();
+        print();
+    }
+    
+    private void shiftLeftTwo() {
+        outputOffset = inputOffset * 2;
+    }
+    
+    private void print() {
+        System.out.println("32-bit wire going to Shift Left 2 component " +
+                "holds value : " + inputOffset);
+        System.out.println("32-bit wire coming out from the Shift Left 2 " +
+                "component holds value : " + outputOffset);
+    }
+    
+    public int getOutput() {
+        return outputOffset;
+    }
+}
